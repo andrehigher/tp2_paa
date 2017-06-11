3f87ed9ceb5feeb4d06c5696a24409a3861522db
Andre Gonzaga <andre.gonzaga@bloomingdales.com>
2017-06-11 14:44:50 -0300

    Remove useless function

--- data_structure/base.h
+++ data_structure/base.h
@@ -12,7 +12,6 @@ void dinamico(int argc, char *argv[]);
 void guloso(int argc, char *argv[]);
 Graph* readNodes(FILE *fp);
 void readEdges(Graph *graph, FILE *fp, FILE *fpwrite, float *maxBenefit);
-void printCombination(Graph *graph, int combination[][2], int total, FILE *fpwrite);
 void printCombinationAux(int arr[], int n, int r, Graph *graph, int combination[][2], float *maxBenefit, int total, FILE *fpwrite);
 void combinationUtil(int arr[], int data[], int start, int end, int index, int r, Graph *graph, int combination[][2], float *maxBenefit, int total, FILE *fpwrite);
 
